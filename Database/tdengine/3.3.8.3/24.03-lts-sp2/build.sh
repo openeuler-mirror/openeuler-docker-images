c8b09ae8a4182b9544cea0852c3162900323e3c4
openeuler_bot <zhaiwenjie1@huawei.com>
2025-10-29 03:11:02 +0000

    24.03-lts-sp2 update tdengine to 3.3.8.3

---
+++ Database/tdengine/3.3.8.3/24.03-lts-sp2/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if [ ! -d debug ]; then
+    mkdir debug || echo -e "failed to make directory for build"
+fi
+
+cd debug && cmake .. -DBUILD_TOOLS=true -DBUILD_CONTRIB=true && make -j4
