3e76569c7ac415b1e277dd4e6373341153751ed4
yanzhicong <mryanzhicong@163.com>
2026-04-08 10:42:02 +0800

    fix: Failed to patch

--- Database/tdengine/3.4.1.1/24.03-lts-sp3/build.sh
+++ Database/tdengine/3.4.1.1/24.03-lts-sp3/build.sh
@@ -4,4 +4,4 @@ if [ ! -d debug ]; then
     mkdir debug || echo -e "failed to make directory for build"
 fi
 
-cd debug && cmake .. -DBUILD_TOOLS=true -DBUILD_CONTRIB=true && make -j4
+cd debug && cmake .. -DBUILD_TOOLS=true -DBUILD_CONTRIB=true -DCMAKE_INSTALL_LIBDIR=lib64  && make -j4
