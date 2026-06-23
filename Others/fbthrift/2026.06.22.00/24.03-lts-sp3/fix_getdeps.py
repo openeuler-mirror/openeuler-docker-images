57e6e5cef36cd5177de61945993f84d3198717df
infra_team <zhaiwenjie1@huawei.com>
2026-06-23 08:14:50 +0800

    24.03-lts-sp3 update

---
+++ Others/fbthrift/2026.06.22.00/24.03-lts-sp3/fix_getdeps.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import re
+
+# 1. 修复 openEuler 发行版识别
+f = 'build/fbcode_builder/getdeps/getdeps_platform.py'
+c = open(f).read()
+c = c.replace(
+    '("fedora", "centos", "centos_stream", "rocky", "alma")',
+    '("fedora", "centos", "centos_stream", "rocky", "alma", "openeuler")'
+)
+open(f, 'w').write(c)
+
+# 2. 跳过 libaio 哈希校验
+f2 = 'build/fbcode_builder/getdeps/fetcher.py'
+c2 = open(f2).read()
+open(f2, 'w').write(re.sub(
+    r'def _verify_hash\(self\):.*?(?=\n    def )',
+    'def _verify_hash(self):\n        pass',
+    c2,
+    flags=re.DOTALL
+))
